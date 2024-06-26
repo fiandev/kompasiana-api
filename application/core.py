aa77e3aa4b525da9552999f4b5dc85f4c58446a2
fiandev <fiandev@programmer.net>
2024-06-26 14:36:35 +0700

    feat: add flask-cors module

--- application/core.py
+++ application/core.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 from application.config import config
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__, template_folder="./resources/views")
 app.secret_key = config["key"]
 app.config['SECRET_KEY'] = config["key"]
+CORS(app)
